56e8bd82772c1d953739f774fa654fcb4c8aa396
Glen Souza <glenster75@gmail.com>
2022-12-07 19:58:25 -0800

    Update release.ps1

--- releasePipeline/release.ps1
+++ releasePipeline/release.ps1
@@ -9,10 +9,10 @@ $body = "{
 `n        `"CountyName`": `"MasterCounty`",
 `n        `"FarmId`": `"521`",
 `n        `"Prefix`": `"MasterPrefix`",
-`n        `"ImageLabel`": `"Ultimate`"
+`n        `"ImageLabel`": `"Ultimate`",
+`n        `"Test`": `"- 001|1|CountyA`n- 002|1|CountyB`n- 003|2|CountyC`"
 `n    }
-`n}
-`n"
+`n}"
 
 $response = Invoke-RestMethod 'https://dev.azure.com/glenster75/jcc%20Bicep%20Demo/_apis/pipelines/23/runs?&api-version=6.1-preview.1' -Method 'POST' -Headers $headers -Body $body
 $response | ConvertTo-Json
